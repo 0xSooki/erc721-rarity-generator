e7927776e5a8af1b87e5d227f562de6118e701a5
Bence Fekete <fekete965@gmail.com>
2022-10-04 20:52:46 +0100

    Define createSpinner util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,6 @@
+const cliSpinners = require('cli-spinners');
 const fetch = require('node-fetch');
+const { stdout } = require('process');
 
 const resolveLink = (url) => {
   if (!url || !url.includes('ipfs://')) return url;
@@ -75,8 +77,61 @@ const getNftImage = async (tokenUri) => {
   }
 };
 
+const createSpinner = (id) => {
+  const state = {
+    id,
+    affix: null,
+    currentFrame: 0,
+    spinnerRef: null
+  };
+
+  const start = (affixOpt) => {
+    if (state.spinnerRef === null) {
+      state.spinnerRef = setInterval(() => {
+        const { currentFrame } = state;
+        state.affix = affixOpt ? affixOpt : null;
+
+        stdout.clearLine();
+        stdout.cursorTo(0);
+
+        const text = affixOpt
+          ? `${affixOpt} ${cliSpinners.dots.frames[currentFrame]} `
+          : `${cliSpinners.dots.frames[currentFrame]} `;
+        stdout.write(text);
+
+        state.currentFrame = (currentFrame + 1) % cliSpinners.dots.frames.length;
+      }, cliSpinners.dots.interval);
+    } else {
+      stdout.write(`\nSpinner ${state.id} is already running\n`);
+    }
+  };
+
+  const stop = () => {
+    if (state.spinnerRef) {
+      clearInterval(state.spinnerRef);
+      if (state.affix) {
+        stdout.cursorTo(state.affix.length);
+        stdout.write('  \n');
+      } else {
+        stdout.clearLine();
+        stdout.cursorTo(0);
+      }
+
+      state.currentFrame = 0;
+      state.affix = null;
+    }
+  };
+
+  return Object.freeze({
+    id,
+    start,
+    stop
+  });
+};
+
 module.exports = {
   calculateTotalRaritybase,
+  createSpinner,
   extractTraitsAndValues,
   generateTally,
   getNftImage,
