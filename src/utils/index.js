e23cb3f184b1fa556b536819db6e5de975293243
Bence Fekete <fekete965@gmail.com>
2022-10-05 23:56:39 +0100

    Add createErrorHandler to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -134,8 +134,55 @@ const createSpinner = (id) => {
   });
 };
 
+const createErrorHandler = () => {
+  const state = {
+    errors: []
+  };
+
+  const printErrors = () => {
+    if (state.errors.length === 0) {
+      return stdout.write('⚠️ No error stored ⚠️ \n');
+    }
+
+    state.errors.forEach(({ keys, tokenId, validationError }) => {
+      stdout.write(`❗ Validation error occured for token #${tokenId} ❗\n`);
+      keys.forEach((key) => {
+        const error = validationError.errors[key]?.message;
+
+        if (error) {
+          stdout.write(`❗ ${error} ❗\n`);
+        }
+      });
+      stdout.write('\n');
+    });
+  };
+
+  const storeErrors = (errors) => {
+    state.errors = errors;
+    return true;
+  };
+
+  const addError = (error) => {
+    state.errors = state.errors.concat(error);
+    return true;
+  };
+
+  const getErrors = () => state.errors;
+
+  const hasError = () => state.errors.length > 0;
+
+  return Object.freeze({
+    addError,
+    getErrors,
+    hasError,
+    printErrors,
+    storeErrors
+  });
+};
+
 module.exports = {
   calculateTotalRaritybase,
+  createErrorHandler,
   createSpinner,
   extractTraitsAndValues,
   generateTally,
